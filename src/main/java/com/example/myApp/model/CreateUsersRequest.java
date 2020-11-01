1da308a4ca9487947b3c33c949395f9994f4bd71
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2020-11-01 20:24:38 +0900

    validationの追加

--- src/main/java/com/example/myApp/model/CreateUsersRequest.java
+++ src/main/java/com/example/myApp/model/CreateUsersRequest.java
@@ -13,36 +13,28 @@ import java.io.Serializable;
 public class CreateUsersRequest implements Serializable {
     private static final long serialVersionUID = 1L;
 
-//    @NotBlank(message = "must not be empty")
+    @NotNull(message = "must not be empty")
+    @Size(min = 1, max = 20, message = "must be 1 to 20 characters")
     private String userName;
 
-//    @NotBlank(message = "must not be empty")
+    @NotNull(message = "must not be empty")
+    @Size(min = 1, max = 20, message = "must be 1 to 20 characters")
     private String firstName;
 
-//    @NotBlank(message = "must not be empty")
+    @NotNull(message = "must not be empty")
+    @Size(min = 1, max = 20, message = "must be 1 to 20 characters")
     private String lastName;
 
-//    @NotBlank(message = "must not be empty")
+    @NotNull(message = "must not be empty")
     private String email;
 
-//    @NotBlank(message = "must not be empty")
+    @NotNull(message = "must not be empty")
     private String password;
 
-//    @NotBlank(message = "must not be empty")
+    @NotNull(message = "must not be empty")
     private String phone;
 
-//    @NotBlank(message = "must not be empty")
+    @NotNull(message = "must not be empty")
     private String imageUrl;
 
-//    @NotBlank(message = "must not be empty")
-//    @Pattern(regexp = "[0-9\\-]{9,16}[0-9X]", message = "is incorrect")
-//    private String isbn;
-//
-//    @NotBlank(message = "must not be empty")
-//    @Size(min = 1, max = 30, message = "must be 1 to 30 characters")
-//    private String title;
-//
-//    @NotNull(message = "must not be empty")
-//    @Pattern(regexp="[0-9]{1,10}", message="is not proper number")
-//    private String price;
 }
